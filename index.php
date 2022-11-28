78b786944f85ae72538e3d8dd8d8bc688684e7a9
php_man <php_man@git.com>
2022-11-28 12:41:25 +0300

    added new files

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+  /*class Index {
+  public function helloworld(){
+    echo "hello world";
+  }
+
+}*/
+// $londonTime = new DateTime('now');
+// // var_dump($londonTime->getTimezone());
+// var_dump($londonTime->getTimezone()->getName());
+
+// $g = 7;
+// echo "$g" . 'is';
+// var_dump($g);
+
+// function newLine() {
+//   echo "\n";
+// }
+
+// function myFunc($b) {
+//   echo 1 + $b;
+//   newLine();
+// }
+// myFunc("true");
+
+// /**
+//   * @param int $a
+//   * @param int $b
+//   * @param string $operation
+//   * @param int int
+//  */
+
+// function calculator(int $a, int $b, string $operation = "plus"): int  {
+//   if($operation === 'plus') {
+//     return $a + $b;
+//   }
+//   if($operation === 'minus') {
+//     return $a - $b;
+//   }
+//   return 7;
+// }
+
+// echo "1 minus 2: " . calculator(1, 2, 'text'); 
+
+// const P = 7;
+// var_dump(P);
+ // isTrue(is_null('null'));
+
+  // function isTrue($var) {
+  //   if($var)
+  //     echo 'true';
+  //   else {
+  //     echo 'false';
+  //   }
+  // }
+  // var_dump('1.5');
+
+
+  // $string = '4';
+  // echo $string . true;
+ 
+  // $n = '8л5па54';
+  // print(sqrt(null)); 
+  // echo(sqrt(null)); 
+  // $array = [2, 4, 5];
+  // var_dump(implode('dfsd'));
+  // echo implode(' ', $array);
+
+  // $str = '';
+  // var_dump((boolean) $str);
+
+  $string = "abc";
+  $stringEmpty = "";
+
+  // isTrue($string);
+  // isTrue($stringEmpty);
+
+  // echo "I have " . $string . " apples";
+  
+  // var_dump(is_null($int));
+
+  // isTrue([]);
+  echo $string;
